da1a1d4912028c456e0a9876931b8098dbef3be3
Riya A <riya.a.bangera@gmail.com>
2025-12-08 17:58:00 +0530

    Update vite.config.ts

--- 05_Website/myntra-engagement-analytics/vite.config.ts
+++ 05_Website/myntra-engagement-analytics/vite.config.ts
@@ -2,14 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
-
 import { miaodaDevPlugin } from "miaoda-sc-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
+  base: '/Myntra_HR_Analytics_Project/',  
+  plugins: [
+    react(), 
+    svgr({
       svgrOptions: {
-        icon: true, exportType: 'named', namedExport: 'ReactComponent', }, }), miaodaDevPlugin()],
+        icon: true, 
+        exportType: 'named', 
+        namedExport: 'ReactComponent', 
+      }, 
+    }), 
+    miaodaDevPlugin()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
